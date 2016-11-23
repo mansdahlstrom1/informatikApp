6d0eab15653a07a1b887d21b3cf44748ba7e6ab4
Måns Dahlström <mans@mansdahlstrom.se>
2016-11-23 14:36:43 +0100

    Inital commit, populating app with data

--- informatikApp/www/js/app.js
+++ informatikApp/www/js/app.js
@@ -50,31 +50,32 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
   .state('tab.account', {
     url: '/account',
     views: {
       'tab-account': {
         templateUrl: 'templates/tab-account.html',
         controller: 'AccountCtrl'
+      }
+    }
+  })
+
+  .state('tab.map', {
+    url: '/map',
+    views: {
+      'tab-map': {
+        templateUrl: 'templates/tab-map.html',
+        controller: 'MapCtrl'
+      }
+    }
+  })
+
+  .state('tab.schedule', {
+    url: '/schedule',
+    views: {
+      'tab-schedule': {
+        templateUrl: 'templates/tab-schedule.html' ,
+        controller: 'ScheduleCtrl'
       }
     }
   });
